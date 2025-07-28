a2cf78e4bee78597ed5e64de3aee5536781ffa3d
Arjun N <arjn@deloitte.com>
2025-07-28 08:50:49 +0000

    Debug - Web Application

--- sales/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["com/deloitte/mdg/sales/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- sales/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"com/deloitte/mdg/sales/test/unit/controller/overview.controller"
-], function () {
-	"use strict";
-});
